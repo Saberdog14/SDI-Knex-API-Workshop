35e269a8525deb9e39aba0477b2b75e4b7e1aa1b
Jackson <Jacksonwgray14@gmail.com>
2026-03-19 13:54:15 -0600

    Added basic express server, knex schema creation and seeds. More to come

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const port = 8080;
 const app = express();
 
-app.get("/", (req, res) => res.status(200).send("beginning here"));
+app.get("/", (req, res) => res.status(200).send("Beginning here"));
+
+app.get("/planes", (req, res) =>
+  res.status(200).send("Plane data will be loaded here"),
+);
 
 app.listen(port, () => console.log(`Server listening on ${port}.`));
